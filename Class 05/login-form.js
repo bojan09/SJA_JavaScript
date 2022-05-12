5a2e593e0d2b887721f7c203b54d981ed02de7b1
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2022-05-12 22:07:21 +0200

    Commiting Class 05

---
+++ Class 05/login-form.js
@@ -0,0 +1,8 @@
+function onSubmit(event) {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+}
+
+const usernameChanged = () => {
+  console.log(document.getElementById("username").value);
+};
